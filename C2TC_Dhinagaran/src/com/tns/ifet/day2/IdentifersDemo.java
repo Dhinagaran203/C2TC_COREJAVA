043c48b3d20664b6ab43195ffb916f2679365d7c
kamal <kamal@DESKTOP-94KNU9C>
2024-10-26 20:40:59 +0530

    Second Commit

---
+++ C2TC_Dhinagaran/src/com/tns/ifet/day2/IdentifersDemo.java
@@ -0,0 +1,13 @@
+package com.tns.ifet.day2;
+public class IdentifersDemo 
+{
+	public static void main(String[] args) 
+	{
+		int $num=20;
+		System.out.println("Value of the number variable is :"+$num);
+		String std_name="Dhinagaran";
+		System.out.println("Value is :"+std_name);
+
+	}
+
+}
