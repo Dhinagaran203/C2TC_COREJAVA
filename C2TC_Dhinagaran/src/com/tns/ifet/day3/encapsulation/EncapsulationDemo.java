415094f7791511e89bc58fd459eccb4ff16a0c18
kamal <kamal@DESKTOP-94KNU9C>
2024-11-06 12:26:58 +0530

    Third Commit

---
+++ C2TC_Dhinagaran/src/com/tns/ifet/day3/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,16 @@
+package com.tns.ifet.day3.encapsulation;
+public class EncapsulationDemo 
+{
+	public static void main(String[] args) 
+	{
+		//object creation 
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Dhinagaran");
+		obj.setAge(20);
+		System.out.println(obj);
+
+
+	}
+
+}
---
+++ C2TC_Dhinagaran/src/com/tns/ifet/day4/singleinheritance/SLInheritanceDemo.java
@@ -0,0 +1,11 @@
+package com.tns.ifet.day4.singleinheritance;
+public class SLInheritanceDemo 
+{
+	public static void main(String[] args) 
+	{
+		Student student = new Student("Dhinagaran", "1234567890", "Chennai", 989898989L, 10, "City College");
+		System.out.println(student);
+
+	}
+
+}
